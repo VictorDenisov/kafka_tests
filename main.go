132b76a46a5367d81f13ef8d76e0b9f0b661a207
Victor Denisov <denisovenator@gmail.com>
2019-03-19 21:01:31 -0700

    More debugging

--- main.go
+++ main.go
@@ -20,13 +20,15 @@ func main() {
 	partition := flag.Int("partition", 0, "Partition number for reader and writer")
 	reader := flag.Bool("reader", false, "Reader option")
 	connReader := flag.Bool("connReader", false, "Conn Reader option")
+	bugProducer := flag.Bool("bugProducer", false, "Bug Producer option")
+	bugConsumer := flag.Bool("bugConsumer", false, "Bug Consumer option")
 	unpart := flag.Bool("unpart", false, "Unpartitioned reader")
 	v2test := flag.Bool("v2test", false, "v2 test")
 	versions := flag.Bool("versions", false, "versions")
 	topic := flag.String("topic", "test_topic", "Topic")
 	flag.Parse()
 
-	count := b2i(*writer) + b2i(*reader) + b2i(*unpart) + b2i(*v2test) + b2i(*versions) + b2i(*connReader)
+	count := b2i(*writer) + b2i(*reader) + b2i(*unpart) + b2i(*v2test) + b2i(*versions) + b2i(*connReader) + b2i(*bugConsumer) + b2i(*bugProducer)
 	if count != 1 {
 		fmt.Printf("We need exactly one command option\n")
 		os.Exit(1)
@@ -42,6 +44,12 @@ func main() {
 	case *connReader:
 		fmt.Printf("This is connReader. Partition %v\n", *partition)
 		ConnReader(*topic, *partition)
+	case *bugProducer:
+		fmt.Printf("This is bugProducer. Partition %v\n", *partition)
+		BugProducer(*topic, *partition)
+	case *bugConsumer:
+		fmt.Printf("This is bugConsumer. Partition %v\n", *partition)
+		BugConsumer(*topic, *partition)
 	case *unpart:
 		fmt.Printf("This is unpart\n")
 		UnpartitionedReader(*topic)
@@ -147,12 +155,13 @@ func b2i(x bool) int {
 
 func Reader(topic string, partition int) {
 	r := k.NewReader(k.ReaderConfig{
-		Brokers:   []string{"kafka:9092"},
+		Brokers:   []string{"localhost:9092"},
 		Topic:     topic,
 		Partition: 0,
 		//MaxWait:  10 * time.Millisecond,
-		MinBytes: 1,
-		MaxBytes: 1000,
+		MinBytes:       1,
+		MaxBytes:       1000,
+		IsolationLevel: k.ReadCommitted,
 	})
 	defer r.Close()
 
@@ -244,6 +253,39 @@ func Writer(topic string, partition int) {
 		}
 
 	}
+
+	conn.Close()
+}
+
+func BugConsumer(topic string, partition int) {
+	conn, _ := k.DialLeader(context.Background(), "tcp", "localhost:9092", topic, partition)
+
+	conn.SetReadDeadline(time.Now().Add(10 * time.Second))
+
+	batch := conn.ReadBatch(1, 1e6) // fetch 10KB min, 1MB max
+
+	b := make([]byte, 10e3) // 10KB max per message
+	for {
+		_, err := batch.Read(b)
+		if err != nil {
+			break
+		}
+		fmt.Println(string(b))
+	}
+
+	batch.Close()
+	conn.Close()
+}
+
+var a = `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32. The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.`
+
+func BugProducer(topic string, partition int) {
+	conn, _ := k.DialLeader(context.Background(), "tcp", "localhost:9092", topic, partition)
+
+	conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
+	conn.WriteMessages(
+		k.Message{Value: []byte(a)},
+	)
 
 	conn.Close()
 }
