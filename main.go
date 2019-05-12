22f8e7f7b00af958240cedf476e0391c83360cfb
Victor Denisov <denisovenator@gmail.com>
2019-05-12 10:10:27 -0700

    Remove bug producer/consumer

--- main.go
+++ main.go
@@ -48,12 +48,6 @@ func main() {
 	case *connReader:
 		fmt.Printf("This is connReader. Partition %v\n", *partition)
 		ConnReader(*topic, *partition)
-	case *bugProducer:
-		fmt.Printf("This is bugProducer. Partition %v\n", *partition)
-		BugProducer(*topic, *partition)
-	case *bugConsumer:
-		fmt.Printf("This is bugConsumer. Partition %v\n", *partition)
-		BugConsumer(*topic, *partition)
 	case *unpart:
 		fmt.Printf("This is unpart\n")
 		UnpartitionedReader(*topic)
@@ -279,39 +273,6 @@ func Writer(topic string, partition int) {
 		}
 
 	}
-
-	conn.Close()
-}
-
-func BugConsumer(topic string, partition int) {
-	conn, _ := k.DialLeader(context.Background(), "tcp", "localhost:9092", topic, partition)
-
-	conn.SetReadDeadline(time.Now().Add(10 * time.Second))
-
-	batch := conn.ReadBatch(1, 1e6) // fetch 10KB min, 1MB max
-
-	b := make([]byte, 10e3) // 10KB max per message
-	for {
-		_, err := batch.Read(b)
-		if err != nil {
-			break
-		}
-		fmt.Println(string(b))
-	}
-
-	batch.Close()
-	conn.Close()
-}
-
-var a = `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32. The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.`
-
-func BugProducer(topic string, partition int) {
-	conn, _ := k.DialLeader(context.Background(), "tcp", "localhost:9092", topic, partition)
-
-	conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
-	conn.WriteMessages(
-		k.Message{Value: []byte(a)},
-	)
 
 	conn.Close()
 }
